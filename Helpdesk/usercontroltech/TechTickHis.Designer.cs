54848ec7e5af42b174d8fc2473ad0db02fc831b5
GetFast <GetFast@DESKTOP-LEAO6C9>
2023-12-28 12:14:43 +0100

    update tecktickhis

--- Helpdesk/usercontroltech/TechTickHis.Designer.cs
+++ Helpdesk/usercontroltech/TechTickHis.Designer.cs
@@ -29,35 +29,51 @@
         private void InitializeComponent()
         {
             HistoriqueGridView = new DataGridView();
+            label1 = new Label();
             ((System.ComponentModel.ISupportInitialize)HistoriqueGridView).BeginInit();
             SuspendLayout();
             // 
             // HistoriqueGridView
             // 
+            HistoriqueGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             HistoriqueGridView.BackgroundColor = SystemColors.Control;
             HistoriqueGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            HistoriqueGridView.Location = new Point(0, 0);
+            HistoriqueGridView.Location = new Point(0, 85);
             HistoriqueGridView.Name = "HistoriqueGridView";
+            HistoriqueGridView.ReadOnly = true;
             HistoriqueGridView.RowHeadersWidth = 51;
             HistoriqueGridView.RowTemplate.Height = 29;
-            HistoriqueGridView.Size = new Size(800, 583);
+            HistoriqueGridView.Size = new Size(800, 498);
             HistoriqueGridView.TabIndex = 0;
             HistoriqueGridView.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(185, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(395, 54);
+            label1.TabIndex = 1;
+            label1.Text = "Historique de ticket";
             // 
             // TechTickHis
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(label1);
             Controls.Add(HistoriqueGridView);
             Name = "TechTickHis";
             Size = new Size(800, 583);
             Load += TechTickHis_Load;
             ((System.ComponentModel.ISupportInitialize)HistoriqueGridView).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView HistoriqueGridView;
+        private Label label1;
     }
 }
