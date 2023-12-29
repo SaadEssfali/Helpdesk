9c3fb8e331be070900c5a2a0a285b2c027efafb1
GetFast <GetFast@DESKTOP-LEAO6C9>
2023-12-29 10:07:54 +0100

    update du tickControlFlow

--- Helpdesk/usercontroltech/TickControlflow.cs
+++ Helpdesk/usercontroltech/TickControlflow.cs
@@ -16,5 +16,10 @@ namespace Helpdesk.usercontroltech
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
